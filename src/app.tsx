2ed8d9f09044159f53f123ad056c3c72b237708c
Wesley Medeiros Ramalho <wesley.medeiros6938@gmail.com>
2024-02-06 20:28:44 -0300

    creation of modal note card

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,13 @@
 import Logo from "./assets/logo-nlw-expert.svg";
+import { NewNoteCard } from "./components/new-note-card";
 import { NoteCard } from "./components/note-card";
 
 export function App() {
+
+  const note = {
+    date: new Date(),
+    content: 'Hello, World!'
+  }
 
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
@@ -17,17 +23,9 @@ export function App() {
         <div className="h-px bg-slate-700"/>
 
         <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
-          <div className="rounded-md bg-slate-700 p-5 space-y-3">
-            <span className="text-sm font-medium text-slate-200">
-              Adicionar nota
-              </span>
-            <p className="text-sm leading-6 text-slate-400">
-              Grave uma nota em audio que será convertida para texto automaticamente
-              </p>
-          </div>
-
-          <NoteCard />
-          <NoteCard />
+          <NewNoteCard />
+          <NoteCard note={note} />
+ 
         </div>
     </div>
   )
