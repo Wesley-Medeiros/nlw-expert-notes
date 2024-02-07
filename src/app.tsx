896b86a1809395b9f6be1f55a2b78c9e54fc914f
Wesley Medeiros Ramalho <wesley.medeiros6938@gmail.com>
2024-02-07 19:18:47 -0300

    aplication search

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import Logo from "./assets/logo-nlw-expert.svg";
 import { NewNoteCard } from "./components/new-note-card";
 import { NoteCard } from "./components/note-card";
@@ -10,7 +10,9 @@ interface Note {
 }
 
 export function App() {
-const [notes, setNotes] = useState<Note[]>(() => {
+  const [search, setSearch] = useState('')
+
+  const [notes, setNotes] = useState<Note[]>(() => {
   const notesOnStorage = localStorage.getItem('notes')
 
   if (notesOnStorage) {
@@ -35,6 +37,16 @@ function onNoteCreated(content: string) {
 
 }
 
+function handleSearch(event: ChangeEvent<HTMLInputElement>) {
+  const query = event.target.value
+
+  setSearch(query)
+}
+
+const filteredNots = search !== ''
+  ? notes.filter(notes => notes.content.toLocaleLowerCase().includes(search.toLocaleLowerCase()))
+  : notes
+
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
       <img src={Logo} alt="nlw Expert" />
@@ -43,6 +55,7 @@ function onNoteCreated(content: string) {
           type="text"
           placeholder='Busque em suas notas...' 
           className="w-full bg-transparent text-3xl font-semibold tracking-tighter outline-none placeholder:text-slate-500"
+          onChange={handleSearch}
           />
         </form>
 
@@ -50,7 +63,7 @@ function onNoteCreated(content: string) {
 
         <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
           <NewNoteCard onNoteCreated={onNoteCreated} />
-          {notes.map(note => {
+          {filteredNots.map(note => {
             return <NoteCard key={note.id} note={note} />
           })}
  
